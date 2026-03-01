47fc460f1af6fa36ac0b251ce79f1ccb357eb035
Crispy <junkstuff7489@gmail.com>
2026-02-28 23:31:29 -0500

    generateInstallScript, index, void: implement Void support

--- src/lib/scripts/index.ts
+++ src/lib/scripts/index.ts
@@ -1,12 +1,15 @@
-
-
-export { escapeShellString, getSelectedPackages, type PackageInfo } from './shared';
-export { generateUbuntuScript } from './ubuntu';
-export { generateDebianScript } from './debian';
-export { generateArchScript } from './arch';
-export { generateFedoraScript } from './fedora';
-export { generateOpenSUSEScript } from './opensuse';
-export { generateNixConfig } from './nix';
-export { generateFlatpakScript } from './flatpak';
-export { generateSnapScript } from './snap';
-export { generateHomebrewScript } from './homebrew';
+export {
+  escapeShellString,
+  getSelectedPackages,
+  type PackageInfo,
+} from "./shared";
+export { generateUbuntuScript } from "./ubuntu";
+export { generateDebianScript } from "./debian";
+export { generateArchScript } from "./arch";
+export { generateFedoraScript } from "./fedora";
+export { generateOpenSUSEScript } from "./opensuse";
+export { generateNixConfig } from "./nix";
+export { generateVoidScript } from "./void";
+export { generateFlatpakScript } from "./flatpak";
+export { generateSnapScript } from "./snap";
+export { generateHomebrewScript } from "./homebrew";
